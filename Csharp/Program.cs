ff92a29b2c72f8367344bb4c5e6d65829ed730f4
Денис С <Den25051999@gmail.com>
2021-06-07 23:16:03 +0300

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 1;
+            var sharp = new List<string>() { "C", "s", "h", "a", "r", "p" };
+            try
+            {
+                Parallel.For(
+                        0,
+                        6,
+                        options,
+                        (i) =>
+                        {
+                            Console.WriteLine("{0}", sharp[i]);
+                        }
+                    );
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Parallel.For has thrown the following (unexpected) exception:\n{0}", e);
+            }
+            
+        }
+    }
+}
+
