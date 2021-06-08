228207ab9c6b2ada9e82e0fc13114756a1c14a84
Денис С <Den25051999@gmail.com>
2021-06-08 20:01:48 +0300

    Another

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -8,26 +8,18 @@ namespace Csharp
 {
     class Program
     {
-        static AutoResetEvent waitHandler = new AutoResetEvent(true);
-        static List<char> sharp = new List<char>() { 'C', 's', 'h', 'a', 'r', 'p' };
+        static async void Print()
+        {
+            List<char> sharp = new List<char>() { 'C', 's', 'h', 'a', 'r', 'p' };
+            foreach (var letter in sharp)
+            {
+                await Task.Factory.StartNew(() => Console.WriteLine(letter));
+            }
+        }
         static void Main(string[] args)
         {
-
-            for (int i = 0; i < sharp.Count; i++)
-            {
-                Thread myThread = new Thread(Count);
-                myThread.Name = $"{i}";
-                myThread.Start(sharp[i]);
-            }
-
-            Console.ReadLine();
-        }
-        public static void Count(object letter)
-        {
-            if (Convert.ToInt32(Thread.CurrentThread.Name) > 0)
-            waitHandler.WaitOne(3000, letter.ToString().StartsWith(sharp[Convert.ToInt32(Thread.CurrentThread.Name) - 1]));
-            Console.WriteLine($"{Thread.CurrentThread.Name}: {letter}");
-            waitHandler.Set();
+            Print();
+            Console.Read();
         }
     }
 }
